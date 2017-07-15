f6387958e9677dbbc8fef98b73490c8e58c40853
yinsuo <yourshell@users.noreply.github.com>
2017-07-15 19:11:53 +0800

    Update copy.sh

--- wp/state/copy.sh
+++ wp/state/copy.sh
@@ -20,3 +20,19 @@ chown www:www $wwwroot/index.html
 
 rm -rf $wwwroot/tmphtml/*
 
+sed -i "s/index.php?p=306/non-repeat\/index.html/g" `grep -rl "index.html" $wwwroot`
+sed -i "s/index.php?p=9/ikev2-vpn\/index.html/g" `grep -rl "index.html" $wwwroot`
+sed -i "s/href=\"..\/..\/..\/..\/ip\/index.html/href=\"https:\/\/ys138.win\/ip\//g" `grep -rl "index.html" $wwwroot`
+sed -i "s/href=\"..\/..\/ip\/index.html/href=\"https:\/\/ys138.win\/ip\//g" `grep -rl "index.html" $wwwroot`
+
+sed -i "s/href='wp-content\/themes\/dmeng2.0\/css/href='https:\/\/yisuo.b0.upaiyun.com\/wp-content\/themes\/dmeng2.0\/css/g" "$wwwroot/index_cn.html"
+sed -i "s/src='wp-includes\/js\/jquery/src='https:\/\/yisuo.b0.upaiyun.com\/wp-includes\/js\/jquery/g" "$wwwroot/index_cn.html"
+sed -i "s/src='wp-content\/themes\/dmeng2.0\/js/src='https:\/\/yisuo.b0.upaiyun.com\/wp-content\/themes\/dmeng2.0\/js/g" "$wwwroot/index_cn.html"
+
+cd /root/
+rm -rf /root/2017
+wget -r -p -np -k https://ys138.win/wp-content/uploads/2017/07/
+mv ys138.win/wp-content/uploads/2017/ /root/
+rm -rf /root/ys138.win
+rm -rf /root/2017/index.html*
+cp -r 2017 $wwwroot/wp-content/uploads/
