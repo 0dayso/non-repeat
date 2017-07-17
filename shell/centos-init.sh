03a861caf08873bf8a2def1ccfa723a62dbf5e85
yinsuo <yourshell@users.noreply.github.com>
2017-07-17 15:37:29 +0800

    Add files via upload

--- shell/centos-init.sh
+++ shell/centos-init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 www="/root/www"
 ftppub="/root/pub"
 ip=`who am i | awk '{print $5}' | sed 's/(//g' | sed 's/)//g'`
@@ -7,6 +8,7 @@ ip=`who am i | awk '{print $5}' | sed 's/(//g' | sed 's/)//g'`
 # 脚本所在目录
 basepath=$(cd `dirname $0`; pwd)
 cd $basepath
+cd /root/
 
 if egrep "CentOS release 6" /etc/redhat-release > /dev/null
 then
@@ -17,11 +19,14 @@ else
 fi
 
 echo "" && echo "======== system update ========" && echo ""
+# 系统升级
 yum update
 yum install epel-release
+yum update
 
 clear
 echo "" && echo "======== install chinese-support ========" && echo ""
+# 安装中文环境
 yum -y groupinstall chinese-support
 touch /etc/sysconfig/i18n
 echo LANG="zh_CN.UTF-8" > /etc/sysconfig/i18n
@@ -32,16 +37,31 @@ echo export LC_ALL="zh_CN.UTF-8" >> /etc/sysconfig/i18n
 
 clear
 echo "" && echo "======== install Development Tools ========" && echo ""
+# 安装编译环境和相关的库
 yum install -y wget curl git vim zip unzip
 yum install -y libjpeg-devel libpng-devel libtiff-devel freetype-devel pam-devel gettext-devel pcre-devel
-yum install -y libxml2 libxml2-devel libxslt libxslt-devel
+yum install -y libxml2 libxml2-devel libxslt libxslt-devel xmlto asciidoc
 yum install -y zlib-devel bzip2-devel xz-devel
-yum install -y libpcap-devel openssl-devel ncurses-devel
-yum install -y flex bison autoconf automake 
-yum groupinstall 'Development Tools'
+yum install -y openssl-devel ncurses-devel libpcap-devel
+yum install -y libtooludns-devel libev-devel
+yum install -y gcc flex bison autoconf automake
+yum -y groupinstall "Development libraries" "Development tools"
+
+clear
+echo "" && echo "======== install autoconf latest version========" && echo ""
+# 安装新版 autoconf
+rpm -e --nodeps autoconf-2.63
+mkdir autoconf && cd autoconf
+wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
+tar -xzf autoconf-2.69.tar.gz 
+cd autoconf-2.69
+./configure 
+make && make install
+cd ../../
 
 clear
 echo "" && echo "======== install python ========" && echo ""
+# 安装 Python2.7
 mkdir python && cd python
 git clone https://github.com/pypa/pip.git
 git clone https://github.com/pypa/setuptools.git
@@ -72,22 +92,95 @@ sed -i 's/#!\/usr\/bin\/python/#!\/usr\/\bin\/python.old/g' "/usr/bin/yum"
 clear
 echo "" && echo "======== install shadowsocks and IKEv2 ========" && echo ""
 mkdir ss && cd ss
-pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+
 touch ss.json
 echo "{"                                     >> ss.json
-echo "\"    server\":\"::\","                >> ss.json
-echo "\"    server_port\":11268,"            >> ss.json
-echo "\"    local_address\": \"127.0.0.1\"," >> ss.json
-echo "\"    local_port\":1080,"              >> ss.json
-echo "\"    password\":\"12345678\","        >> ss.json
-echo "\"    timeout\":300,"                  >> ss.json
-echo "\"    method\":\"aes-256-cfb\","       >> ss.json
-echo "\"    fast_open\": false"              >> ss.json
+echo "    \"server\":\"::\","                >> ss.json
+echo "    \"server_port\":11268,"            >> ss.json
+echo "    \"local_address\": \"127.0.0.1\"," >> ss.json
+echo "    \"local_port\":1080,"              >> ss.json
+echo "    \"password\":\"12345678\","        >> ss.json
+echo "    \"timeout\":300,"                  >> ss.json
+echo "    \"method\":\"aes-256-cfb\","       >> ss.json
+echo "    \"forbidden-ip\":{"                >> ss.json
+echo "        \"110.54.128.0\/17\","         >> ss.json
+echo "        \"112.198.0.0\/16\""           >> ss.json
+echo "    }"                                 >> ss.json
+echo "    \"log-file\": \"\/dev\/null\","    >> ss.json
+echo "    \"fast_open\": false"              >> ss.json
 echo "}"                                     >> ss.json
 
-touch start.sh && chmod +x start.sh
-echo  "ssserver -c ss.json --user nobody -d start" > start.sh
-./start.sh
+touch autoss.sh && chmod +x autoss.sh
+echo "#!/bin/sh"                                                                         >> autoss.sh
+echo "#"                                                                                 >> autoss.sh
+echo "# shadowsocks start\/restart\/stop shadowsocks"                                    >> autoss.sh
+echo "#"                                                                                 >> autoss.sh
+echo "# chkconfig: 2345 85 15"                                                           >> autoss.sh
+echo "# description: start shadowsocks\/ssserver at boot time"                           >> autoss.sh
+echo ""                                                                                  >> autoss.sh
+echo "ssconfig=\/root\/ss\/ss.json"                                                      >> autoss.sh
+echo "start(){"                                                                          >> autoss.sh
+echo "        ssserver -q -c $ssconfig --log-file \/dev\/null --user nobody -d start"    >> autoss.sh
+echo "}"                                                                                 >> autoss.sh
+echo "stop(){"                                                                           >> autoss.sh
+echo "        ssserver -q -c $ssconfig --log-file \/dev\/null --user nobody -d stop"     >> autoss.sh
+echo "}"                                                                                 >> autoss.sh
+echo "restart(){"                                                                        >> autoss.sh
+echo "        ssserver -q -c $ssconfig --log-file \/dev\/null --user nobody -d restart"  >> autoss.sh
+echo "}"                                                                                 >> autoss.sh
+echo ""                                                                                  >> autoss.sh
+echo "case \"$1\" in"                                                                    >> autoss.sh
+echo "start)"                                                                            >> autoss.sh
+echo "        start"                                                                     >> autoss.sh
+echo "        ;;"                                                                        >> autoss.sh
+echo "stop)"                                                                             >> autoss.sh
+echo "        stop"                                                                      >> autoss.sh
+echo "        ;;"                                                                        >> autoss.sh
+echo "restart)"                                                                          >> autoss.sh
+echo "        restart"                                                                   >> autoss.sh
+echo "        ;;"                                                                        >> autoss.sh
+echo "*)"                                                                                >> autoss.sh
+echo "        echo \"Usage: $0 {start|restart|stop}\""                                   >> autoss.sh
+echo "        exit 1"                                                                    >> autoss.sh
+echo "        ;;"                                                                        >> autoss.sh
+echo "esac"                                                                              >> autoss.sh
+
+yum install -y mbedtls-devel libsodium-devel libsodium
+
+# 安装 Libsodium
+export LIBSODIUM_VER=1.0.13
+wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
+tar xvf libsodium-$LIBSODIUM_VER.tar.gz
+pushd libsodium-$LIBSODIUM_VER
+popd
+ldconfig
+./autogen.sh && ./configure --prefix=/usr && make
+make install
+
+# 安装 MbedTLS
+export MBEDTLS_VER=2.5.1
+wget https://tls.mbed.org/download/mbedtls-$MBEDTLS_VER-gpl.tgz
+tar xvf mbedtls-$MBEDTLS_VER-gpl.tgz
+pushd mbedtls-$MBEDTLS_VER
+make SHARED=1 CFLAGS=-fPIC
+make DESTDIR=/usr install
+popd
+ldconfig
+./autogen.sh && ./configure && make
+sudo make install
+
+# 安装 shadowsocks-libev
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
+cd shadowsocks-libev
+git submodule update --init --recursive
+./autogen.sh && ./configure --prefix=/usr && make
+make install 
+cd ..
+
+# 安装 python 版
+# pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+
+./autoss.sh start
 cd ..
 
 mkdir ikev2 && cd ikev2
