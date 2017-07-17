621fad9c6bf9218ea431717585bb148f1df1a33c
yinsuo <yourshell@users.noreply.github.com>
2017-07-17 17:22:21 +0800

    Add files via upload

--- shell/centos-init.sh
+++ shell/centos-init.sh
@@ -86,7 +86,7 @@ cd ..
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 cd ..
-sed -i 's/#!\/usr\/bin\/python/#!\/usr\/\bin\/python2.6/g' "/usr/bin/yum"
+sed -i 's/#!\/usr\/bin\/python/#!\/usr\/\bin\/python.old/g' "/usr/bin/yum"
 
 clear
 echo "" && echo "======== install shadowsocks and IKEv2 ========" && echo ""
@@ -102,33 +102,33 @@ echo "    \"password\":\"12345678\","        >> ss.json
 echo "    \"timeout\":300,"                  >> ss.json
 echo "    \"method\":\"aes-256-cfb\","       >> ss.json
 echo "    \"forbidden-ip\":{"                >> ss.json
-echo "        \"110.54.128.0\/17\","         >> ss.json
-echo "        \"112.198.0.0\/16\""           >> ss.json
+echo "        \"110.54.128.0/17\","          >> ss.json
+echo "        \"112.198.0.0/16\""            >> ss.json
 echo "    }"                                 >> ss.json
-echo "    \"log-file\": \"\/dev\/null\","    >> ss.json
+echo "    \"log-file\": \"/dev/null\","      >> ss.json
 echo "    \"fast_open\": false"              >> ss.json
 echo "}"                                     >> ss.json
 
 touch autoss.sh && chmod +x autoss.sh
 echo "#!/bin/sh"                                                                         >> autoss.sh
 echo "#"                                                                                 >> autoss.sh
-echo "# shadowsocks start\/restart\/stop shadowsocks"                                    >> autoss.sh
+echo "# shadowsocks start/restart/stop shadowsocks"                                      >> autoss.sh
 echo "#"                                                                                 >> autoss.sh
 echo "# chkconfig: 2345 85 15"                                                           >> autoss.sh
-echo "# description: start shadowsocks\/ssserver at boot time"                           >> autoss.sh
+echo "# description: start shadowsocks/ssserver at boot time"                            >> autoss.sh
 echo ""                                                                                  >> autoss.sh
-echo "ssconfig=\/root\/ss\/ss.json"                                                      >> autoss.sh
+echo "ssconfig=/root/ss/ss.json"                                                         >> autoss.sh
 echo "start(){"                                                                          >> autoss.sh
-echo "        ssserver -q -c $ssconfig --log-file \/dev\/null --user nobody -d start"    >> autoss.sh
+echo "        ssserver -q -c \$ssconfig --log-file /dev/null --user nobody -d start"      >> autoss.sh
 echo "}"                                                                                 >> autoss.sh
 echo "stop(){"                                                                           >> autoss.sh
-echo "        ssserver -q -c $ssconfig --log-file \/dev\/null --user nobody -d stop"     >> autoss.sh
+echo "        ssserver -q -c \$ssconfig --log-file /dev/null --user nobody -d stop"       >> autoss.sh
 echo "}"                                                                                 >> autoss.sh
 echo "restart(){"                                                                        >> autoss.sh
-echo "        ssserver -q -c $ssconfig --log-file \/dev\/null --user nobody -d restart"  >> autoss.sh
+echo "        ssserver -q -c \$ssconfig --log-file /dev/null --user nobody -d restart"    >> autoss.sh
 echo "}"                                                                                 >> autoss.sh
 echo ""                                                                                  >> autoss.sh
-echo "case \"$1\" in"                                                                    >> autoss.sh
+echo "case \"\$1\" in"                                                                   >> autoss.sh
 echo "start)"                                                                            >> autoss.sh
 echo "        start"                                                                     >> autoss.sh
 echo "        ;;"                                                                        >> autoss.sh
@@ -139,7 +139,7 @@ echo "restart)"                                                                 
 echo "        restart"                                                                   >> autoss.sh
 echo "        ;;"                                                                        >> autoss.sh
 echo "*)"                                                                                >> autoss.sh
-echo "        echo \"Usage: $0 {start|restart|stop}\""                                   >> autoss.sh
+echo "        echo \"Usage: \$0 {start|restart|stop}\""                                  >> autoss.sh
 echo "        exit 1"                                                                    >> autoss.sh
 echo "        ;;"                                                                        >> autoss.sh
 echo "esac"                                                                              >> autoss.sh
